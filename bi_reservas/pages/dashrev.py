fdbf97a91152ffd2882a81bbb51f6a174f11e95e
gugaibara <gustavo.ibara@360suites.com.br>
2026-01-07 13:20:53 -0300

    Add files via upload

---
+++ bi_reservas/pages/dashrev.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dash Revenue",
+    layout="wide"
+)
+
+st.title("📈 Dash Revenue")
+
+st.info(
+    "Esta página será utilizada para análises avançadas de receita "
+    "(Revenue Management)."
+)
+
+st.divider()
+
+st.markdown("""
+### 🚧 Em construção
+
+Aqui entrarão:
+- Análises de receita
+- Comparativos temporais
+- Indicadores avançados (ADR, RevPAR, Run Rate, etc.)
+""")
